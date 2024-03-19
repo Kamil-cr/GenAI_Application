bc064c0154da47a6f56b2f3c666637c50dcd7277
Syed Kamil Zafar <kamilzafar54@gmail.com>
2024-03-19 16:48:52 +0500

    Add more tests in test_main.py

--- fastapi-todo/tests/test_main.py
+++ fastapi-todo/tests/test_main.py
@@ -48,8 +48,11 @@ def test_post_todos():
         app.dependency_overrides[db_session] = get_session_override 
 
         response = client.post("/todos/?todo=Test")
+        response1 = client.post("/todos/?todo=Test2")
         assert response.status_code == 200
-
+        assert response.json() == {"id": 1, "todo": "Test"} 
+        assert response1.status_code == 200
+        assert response1.json() == {"id": 2, "todo": "Test2"}
 
 def test_patch_todos():
     client = TestClient(app=app)
@@ -69,7 +72,11 @@ def test_patch_todos():
         app.dependency_overrides[db_session] = get_session_override 
 
         response = client.patch("/todos/?id=3&update_todo=Test")
+        response1 = client.patch("/todos/?id=4&update_todo=Test")
         assert response.status_code == 200
+        assert response.json() == {"id": 3, "todo": "Test"}
+        assert response1.status_code == 200
+        assert response1.json() == {"id": 4, "todo": "Test"}
 
 def test_delete_todos():
     client = TestClient(app=app)
@@ -89,5 +96,8 @@ def test_delete_todos():
         app.dependency_overrides[db_session] = get_session_override 
 
         response = client.delete("/todos/?id=3")
+        response1 = client.delete("/todos/?id=4")
         assert response.status_code == 200
-
+        assert response.json() == {"id": None}
+        assert response1.status_code == 200
+        assert response1.json() == {"id": None}
