a4cfd2a251d21d8e7e46eeaa5549fee1118cbebc
Syed Kamil Zafar <kamilzafar54@gmail.com>
2024-04-06 07:02:51 +0500

    Refactor addToCart action to remove unused code and improve error handling, and import GetCart in cart page

--- Ecom/ui/actions/get_cart_product.ts
+++ Ecom/ui/actions/get_cart_product.ts
@@ -1,21 +1,15 @@
 "use server"
-import { IProducts } from "@/components/Products"
 import { cookies } from "next/headers";
 
-export const addToCart = async (size: string, product_data: IProducts) => {
+export const GetCart = async () => {
     const isCookies = cookies().get("access_token")?.value;
     try{
         const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/cart`, {
-            method: "POST",
+            method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 'Authorization': `Bearer ${isCookies}`
             },
-            body: JSON.stringify({
-                product_id: product_data.sku,
-                quantity: 1,
-                product_size: size
-            }),
         })
         const data = await response.json()
         return data
--- Ecom/ui/app/cart/page.tsx
+++ Ecom/ui/app/cart/page.tsx
@@ -1,6 +1,8 @@
+import { GetCart } from '@/actions/get_cart_product'
 import React from 'react'
 
 const page = () => {
+  GetCart()
   return (
     <div className='h-full'></div>
   )
